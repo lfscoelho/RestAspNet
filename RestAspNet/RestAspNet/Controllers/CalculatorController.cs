07505d113b63b658cfd4fd7caaaa6e53617b6344
Luis Coelho <lfscoelho@gmail.com>
2018-11-24 01:39:00 +0000

    Rest calculator

---
+++ RestAspNet/RestAspNet/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAspNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+       
+
+        // GET api/values/5/5
+        [HttpGet("{firstNum}/{secondNum}")]
+        public ActionResult<string> Get(string firstNum, string secondNum)
+        {   
+            if(IsNumeric(firstNum) && IsNumeric(secondNum))
+            {
+                var sum = ConvertToDecimal(firstNum) + ConvertToDecimal(secondNum);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double num;
+
+            bool isNumber = double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out num);
+
+            return isNumber;
+        }
+    }
+}
